8bd59ec6fde9fa49719d6ba6637c4cbe08c1ae04
hazira-rozi <hazirafakhrurrozi@hotmail.com>
2022-10-11 09:02:16 +0700

    Initial Commit

---
+++ app/src/main/java/com/wanda/rekonsiliasibank/MainActivity.java
@@ -0,0 +1,45 @@
+package com.wanda.rekonsiliasibank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView playBrowser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playBrowser = (WebView)findViewById(R.id.playBrowser);
+        WebSettings webSettings = playBrowser.getSettings();
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        playBrowser.setWebViewClient(new CallBack());
+        playBrowser.loadUrl("file:///android_asset/index.html");
+    }
+
+    private class CallBack extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            return false;
+        }
+    }
+    @Override
+    public void onBackPressed(){
+        if(playBrowser !=null && playBrowser.canGoBack()){
+            playBrowser.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
