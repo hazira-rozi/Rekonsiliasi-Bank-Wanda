2e7b2b853ba5642dd5bd772832b82277501882d7
hazira-rozi <hazirafakhrurrozi@hotmail.com>
2022-10-25 15:03:53 +0700

    Using hosted web

--- app/src/main/java/com/wanda/rekonsiliasibank/MainActivity.java
+++ app/src/main/java/com/wanda/rekonsiliasibank/MainActivity.java
@@ -7,17 +7,22 @@ import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
+//import org.mozilla.geckoview.GeckoRuntime;
+import org.mozilla.geckoview.GeckoRuntime;
+import org.mozilla.geckoview.GeckoSession;
+import org.mozilla.geckoview.GeckoView;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView playBrowser;
+private static GeckoRuntime sRuntime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//
         playBrowser = (WebView)findViewById(R.id.playBrowser);
         WebSettings webSettings = playBrowser.getSettings();
         webSettings.setBuiltInZoomControls(true);
@@ -25,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setPluginState(WebSettings.PluginState.ON);
         webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
         playBrowser.setWebViewClient(new CallBack());
-        playBrowser.loadUrl("file:///android_asset/index.html");
+        playBrowser.loadUrl("https://wandaproject.000webhostapp.com/");
     }
 
     private class CallBack extends WebViewClient{
